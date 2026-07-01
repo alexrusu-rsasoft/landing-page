1e013fb8b275fd4f0f161b1cbf1e46c1f293fab0
Rusu, Alex (FDI1_EXTERN) <extern.alex.rusu1@porsche.de>
2026-07-02 00:56:55 +0300

    fix scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { RouterOutlet } from '@angular/router';
   template: '<router-outlet />',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  constructor() {
+    // Angular's anchorScrolling ignores CSS scroll-padding-top / scroll-margin-top.
+    // ViewportScroller.setOffset is the correct way to reserve space for the sticky nav.
+    inject(ViewportScroller).setOffset([0, 80]);
+  }
+}
