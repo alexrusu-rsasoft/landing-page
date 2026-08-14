efaf8314b73c097dfd4fbf2ca8d01c37c699fff7
Rusu, Alex (FDI1_EXTERN) <extern.alex.rusu1@porsche.de>
2026-08-14 23:11:19 +0300

    Update careers API URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   experienceApiUrl:
     'https://script.google.com/macros/s/AKfycbxKhsSJpOdyaXT9AIz9N-wjgecoAqyf-rpUHm3GK4SGrSrlkvGx3lAPJiYmKCVdaBW_/exec',
   careersApiUrl:
-    'https://script.google.com/macros/s/AKfycby8RLRPV_oLtX1j0FdFc3EBMYgVYd_SpmmIuCyRcLbW9MFdV5KAXAVZYGyP0u-jp47c/exec',
+    'https://script.google.com/macros/s/AKfycbwjH6AnfcXnXWFH4K-uKEDd0LsAY9MjboII2Q0C9CPD-QhVHjgJ3wf3JNmrpUcIYk2K/exec',
 };
